dd6ca953526a42c01fca848da9dd82fb6fa624fd
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2020-08-31 00:13:19 +0200

    MainJs para github

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+/*
+Rellenar la configuración con los datos de firebase
+*/
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   render: h => h(App),
